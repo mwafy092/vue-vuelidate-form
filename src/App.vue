85598415f866937fe8039fa1e50716a3df306187
Mustafa Mwafy <66922777+mwafy092@users.noreply.github.com>
2022-06-29 13:15:19 +0200

    adding validation

--- src/App.vue
+++ src/App.vue
@@ -11,22 +11,36 @@
       <div class="form__data__chunk">
         <label for="firstName" class="form-label">
           <span class="form__label">الاسم الاول</span>
+
           <input
             type="text"
-            class="form-control"
+            :class="[
+              'form-control',
+              v$.firstName.required.$invalid && 'form__error--active',
+            ]"
             id="firstName"
-            v-model="formData.firstName"
+            v-model="firstName"
           />
+          <p class="form__error__message" v-if="v$.firstName.required.$invalid">
+            هذه البيانات مطلوبه
+          </p>
         </label>
+
         <label for="lastName" class="form-label">
           <span class="form__label">الاسم الاخير</span>
 
           <input
             type="text"
-            class="form-control"
+            :class="[
+              'form-control',
+              v$.lastName.required.$invalid && 'form__error--active',
+            ]"
             id="lastName"
-            v-model="formData.lastName"
+            v-model="lastName"
           />
+          <p class="form__error__message" v-if="v$.lastName.required.$invalid">
+            هذه البيانات مطلوبه
+          </p>
         </label>
       </div>
       <div class="form__data__chunk">
@@ -34,31 +48,60 @@
           <span class="form__label">البريد الالكتروني</span>
           <input
             type="email"
-            class="form-control"
+            :class="[
+              'form-control',
+              (v$.email.email.$invalid || v$.email.required.$invalid) &&
+                'form__error--active',
+            ]"
             id="email"
             aria-describedby="emailHelp"
-            v-model="formData.email"
+            v-model="email"
           />
+          <p class="form__error__message" v-if="v$.email.required.$invalid">
+            هذه البيانات مطلوبه
+          </p>
+          <p class="form__error__message" v-if="v$.email.email.$invalid">
+            يرجي ادخال البريد الالكتروني بشكل صحيح
+          </p>
         </label>
         <label for="password" class="form-label">
           <span class="form__label">الرقم السري</span>
 
           <input
             type="password"
-            class="form-control"
+            :class="[
+              'form-control',
+              (v$.password.minLength.$invalid ||
+                v$.password.required.$invalid) &&
+                'form__error--active',
+            ]"
             id="password"
-            v-model="formData.password"
+            v-model="password"
           />
+          <p class="form__error__message" v-if="v$.password.required.$invalid">
+            هذه البيانات مطلوبه
+          </p>
+          <p class="form__error__message" v-if="v$.password.minLength.$invalid">
+            يرجي ادخال علي الاقل ٨ حروف او ارقام او رموز
+          </p>
         </label>
         <label for="passwordConfirm" class="form-label">
           <span class="form__label">تاكيد الرقم السري</span>
-
           <input
-            type="passwordConfirm"
-            class="form-control"
-            id="password"
-            v-model="formData.passwordConfirm"
+            type="password"
+            :class="[
+              'form-control',
+              (password !== passwordConfirm ||
+                v$.passwordConfirm.required.$invalid) &&
+                'form__error--active',
+            ]"
+            id="passwordConfirm"
+            v-model="passwordConfirm"
           />
+
+          <p class="form__error__message" v-if="password !== passwordConfirm">
+            يرجي التاكد من ادخال نفس الرقم السري
+          </p>
         </label>
       </div>
       <div class="form__data__chunk">
@@ -66,18 +109,38 @@
           <span class="form__label"> الرقم القومي</span>
           <input
             type="number"
-            class="form-control"
+            :class="[
+              'form-control',
+              (v$.idNumber.required.$invalid ||
+                v$.idNumber.minLength.$invalid ||
+                v$.idNumber.maxLength.$invalid) &&
+                'form__error--active',
+            ]"
             id="idNumber"
-            v-model="formData.idNumber"
+            v-model="idNumber"
           />
+          <p class="form__error__message" v-if="v$.idNumber.required.$invalid">
+            هذه البيانات مطلوبه
+          </p>
+          <p
+            class="form__error__message"
+            v-if="
+              v$.idNumber.minLength.$invalid || v$.idNumber.maxLength.$invalid
+            "
+          >
+            الرقم القومي مكون فقط من ١٤ رقم
+          </p>
         </label>
         <label for="birthDate" class="form-label">
           <span class="form__label">تاريخ الميلاد</span>
           <input
             type="date"
-            class="form-control"
+            :class="[
+              'form-control',
+              v$.birthDate.required.$invalid && 'form__error--active',
+            ]"
             id="birthDate"
-            v-model="formData.birthDate"
+            v-model="birthDate"
           />
         </label>
       </div>
@@ -90,7 +153,7 @@
             name="gender"
             id="gender1"
             value="male"
-            v-model="formData.gender"
+            v-model="gender"
           />
           <label class="form-check-label" for="gender1"> ذكر </label>
         </div>
@@ -101,10 +164,13 @@
             name="gender"
             id="gender2"
             value="female"
-            v-model="formData.gender"
+            v-model="gender"
           />
           <label class="form-check-label" for="gender2"> انثي </label>
         </div>
+        <p class="form__error__message" v-if="v$.gender.required.$invalid">
+          هذه البيانات مطلوبه
+        </p>
       </div>
       <div class="form__data__chunk__image">
         <input id="uploadFile" disabled="disabled" class="form__image__hide" />
@@ -136,46 +202,75 @@
       >
         <h2 class="form__data__chunk__title" v-if="item === 1">
           محل إقامة اساسي
+          <p class="form__error__message" v-if="mainAddressRequired">
+            يرجي ادخال محل الاقامه الاساسي اولا
+          </p>
         </h2>
+
         <h2 class="form__data__chunk__title" v-else>محل إقامة اضافي</h2>
         <div class="form__data__chunk">
           <label for="country" class="form-label">
             <span class="form__label">الدوله</span>
             <input
               type="text"
-              class="form-control"
+              :class="[
+                'form-control',
+                v$.country.required.$invalid && 'form__error--active',
+              ]"
               id="country"
-              v-model="formData.country[index]"
+              v-model="country[index]"
             />
+            <p class="form__error__message" v-if="v$.country.required.$invalid">
+              هذه البيانات مطلوبه
+            </p>
           </label>
           <label for="city" class="form-label">
             <span class="form__label">المدينه</span>
             <input
               type="text"
-              class="form-control"
+              :class="[
+                'form-control',
+                v$.city.required.$invalid && 'form__error--active',
+              ]"
               id="city"
-              v-model="formData.city[index]"
+              v-model="city[index]"
             />
+            <p class="form__error__message" v-if="v$.city.required.$invalid">
+              هذه البيانات مطلوبه
+            </p>
           </label>
           <label for="street" class="form-label">
             <span class="form__label">اسم الشارع</span>
             <input
               type="text"
-              class="form-control"
+              :class="[
+                'form-control',
+                v$.street.required.$invalid && 'form__error--active',
+              ]"
               id="street"
-              v-model="formData.street[index]"
+              v-model="street[index]"
             />
+            <p class="form__error__message" v-if="v$.street.required.$invalid">
+              هذه البيانات مطلوبه
+            </p>
           </label>
           <label for="flatNo" class="form-label">
             <span class="form__label">رقم الشقه</span>
             <input
               type="text"
-              class="form-control"
+              :class="[
+                'form-control',
+                v$.flatNo.required.$invalid && 'form__error--active',
+              ]"
               id="flatNo"
-              v-model="formData.flatNo[index]"
+              v-model="flatNo[index]"
             />
+            <p class="form__error__message" v-if="v$.flatNo.required.$invalid">
+              هذه البيانات مطلوبه
+            </p>
           </label>
         </div>
+
         <div class="toggle__field__buttons">
           <button
             v-if="index === 0"
@@ -185,6 +280,7 @@
             اضافه محل إقامة جديد
             <img src="./assets/add.png" width="20" />
           </button>
+
           <button
             v-if="addressCount > 1 && index !== 0"
             class="toggle__field__button"
@@ -228,14 +324,24 @@
 
             <input
               type="text"
-              class="form-control"
+              :class="[
+                'form-control',
+                (v$.phone.required.$invalid || this.phone[0].length !== 11) &&
+                  'form__error--active',
+              ]"
               placeholder="0101000xxx"
               aria-label="phone"
               name="phone"
               style="margin-left: -30px"
-              v-model="formData.phone[index]"
+              v-model="phone[index]"
             />
           </div>
+          <p class="form__error__message" v-if="v$.phone.required.$invalid">
+            هذه البيانات مطلوبه
+          </p>
+          <p class="form__error__message" v-if="mainPhoneRequired">
+            يرجي ادخال رقم المحمول الاساسي قبل اضافه رقم اخر
+          </p>
         </div>
       </div>
       <button class="form__submit__button" @click.prevent="submitFormData">
@@ -247,34 +353,99 @@
 
 <script>
 import LoaderSpinner from "vue-spinner/src/ClipLoader.vue";
+import useVuelidate from "@vuelidate/core";
+import {
+  required,
+  email,
+  minLength,
+  maxLength,
+  sameAs,
+} from "@vuelidate/validators";
+
 export default {
   components: {
     LoaderSpinner,
   },
+
+  setup() {
+    return { v$: useVuelidate() };
+  },
   data() {
     return {
+      isSubmit: false,
       loading: false,
       img: "",
       addressCount: 1,
       phoneCount: 1,
       phoneCode: "",
-      formData: {
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: "",
-        idNumber: "",
-        birthDate: "",
-        passwordConfirm: "",
-        gender: "",
-        profile: "",
-        country: [],
-        city: [],
-        street: [],
-        flatNo: [],
-        phone: [],
-      },
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      idNumber: "",
+      birthDate: "",
+      passwordConfirm: "",
+      gender: "",
+      profile: "",
+      country: [],
+      city: [],
+      street: [],
+      flatNo: [],
+      phone: [],
+      mainAddressRequired: false,
+      mainPhoneRequired: false,
     };
+  },
+  validations: {
+    email: {
+      required,
+      email,
+    },
+
+    firstName: {
+      required,
+    },
+
+    lastName: {
+      required,
+    },
+
+    password: {
+      required,
+      minLength: minLength(8),
+    },
+
+    passwordConfirm: {
+      required,
+    },
+    idNumber: {
+      required,
+      minLength: minLength(14),
+      maxLength: maxLength(14),
+    },
+    birthDate: {
+      required,
+    },
+    gender: {
+      required,
+    },
+    country: {
+      required,
+    },
+    city: {
+      required,
+    },
+    street: {
+      required,
+    },
+    flatNo: {
+      required,
+    },
+    phone: {
+      required,
+      minLength: minLength(11),
+      maxLength: maxLength(11),
+    },
   },
   mounted() {
     fetch(
@@ -293,12 +464,32 @@ export default {
         this.loading = false;
       }, 3000);
     },
+
     addFormField(value) {
       switch (value) {
         case "address":
+          if (
+            (!this.country.length ||
+              !this.city.length ||
+              !this.street.length ||
+              !this.flatNo.length) &&
+            this.addressCount !== 1
+          ) {
+            this.mainAddressRequired = true;
+            break;
+          }
+          this.mainAddressRequired = false;
           this.addressCount = this.addressCount + 1;
           break;
         case "phone":
+          if (
+            (this.phone.length === 0 || this.phone[0]?.length !== 11) &&
+            this.phoneCode !== 1
+          ) {
+            this.mainPhoneRequired = true;
+            break;
+          }
+          this.mainPhoneRequired = false;
           this.phoneCount = this.phoneCount + 1;
           break;
         default:
@@ -308,15 +499,15 @@ export default {
     removeFormField(value, index) {
       switch (value) {
         case "address":
-          this.formData.country.splice(index, index);
-          this.formData.city.splice(index, index);
-          this.formData.street.splice(index, index);
-          this.formData.flatNo.splice(index, index);
+          this.country.splice(index, index);
+          this.city.splice(index, index);
+          this.street.splice(index, index);
+          this.flatNo.splice(index, index);
           this.addressCount =
             this.addressCount > 1 ? this.addressCount - 1 : this.addressCount;
           break;
         case "phone":
-          this.formData.phone.splice(index, index);
+          this.phone.splice(index, index);
           this.phoneCount =
             this.phoneCount > 1 ? this.phoneCount - 1 : this.phoneCount;
           break;
@@ -325,30 +516,47 @@ export default {
       }
     },
     submitFormData() {
+      this.isSubmit = true;
       let addresses = [];
       if (this.addressCount > 1) {
         for (let i = 0; i < this.addressCount; i++) {
           addresses.push({
-            country: this.formData.country[i],
-            city: this.formData.city[i],
-            street: this.formData.street[i],
-            flatNumber: this.formData.flatNo[i],
+            country: this.country[i],
+            city: this.city[i],
+            street: this.street[i],
+            flatNumber: this.flatNo[i],
           });
         }
       }
       const data = {
-        firstName: this.formData.firstName,
-        lastName: this.formData.lastName,
-        email: this.formData.email,
-        password: this.formData.password,
-        id: this.formData.idNumber,
-        birthDate: this.formData.birthDate,
+        firstName: this.firstName,
+        lastName: this.lastName,
+        email: this.email,
+        password: this.password,
+        id: this.idNumber,
+        birthDate: this.birthDate,
         imgUrl: this.img,
-        gender: this.formData.gender,
+        gender: this.gender,
         address: addresses,
-        phone: this.formData.phone,
+        phone: this.phone,
       };
-      console.log(data);
+
+      if (
+        !this.firstName ||
+        !this.lastName ||
+        !this.email ||
+        !this.password ||
+        !this.id ||
+        !this.birthDate ||
+        !this.img ||
+        !this.gender ||
+        addresses.length !== 0 ||
+        !this.phone
+      ) {
+        console.log("error");
+      } else {
+        console.log("not error");
+      }
     },
   },
 };
